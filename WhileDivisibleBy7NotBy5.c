82dd24c31cc7560b2e3f80d91559173edc82b0d3
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2023-09-12 17:32:57 +0530

    Add files via upload

---
+++ WhileDivisibleBy7NotBy5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<math.h>
+void main() 
+{
+    int i=101;
+    while(i<200)
+    {
+        if((i%7==0) && (i%5!=0))
+        {
+            printf("%d \n",i);
+            i++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
