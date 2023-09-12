f4a534579a4b294298e7283ff0f58215046884b7
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2023-09-12 15:41:54 +0530

    Modified WhilePrint1ToN.c

--- WhilePrint1ToN.c
+++ WhilePrint1ToN.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 void main() 
 {
-    int i=1;
-    while(i<=10)
+    int i=1,n;
+    printf("Enter the last number : ");
+    scanf("%d",&n);
+    
+    while(i<=n)
     {
         printf("%d \n",i);
         i++;
