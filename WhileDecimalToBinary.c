fdc0e51e7a1e1d902f7aa6fb8274aae8c6f0a1f5
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2023-10-02 13:47:08 +0530

    Update WhileDecimalToBinary.c

--- WhileDecimalToBinary.c
+++ WhileDecimalToBinary.c
@@ -1,23 +1,26 @@
-#include <stdio.h>  
-  
-void main()  
-{  
-    int n, bin = 0,rem=0,place = 1;  
-  
-    printf("Enter a decimal number\n");  
-    scanf("%d", &n);  
-  
-    printf("\nBinary = ",n);  
-    while(n!=0)  
+#include <stdio.h>
+void main()
+{
+    int n, c=31, k;
+
+    printf("Enter an integer in decimal number system\n");
+    scanf("%d", &n);
+
+    printf("%d in binary number system is:\n", n);
+
+    while(c >= 0)
     {
-         rem = n%2;
-        n   = n / 2; 
-        bin   = bin + (rem * (place));  
-        
-        place = place * 10;  
-    }  
-    
-    printf("\nBinary value = %d",bin);
-  
-    
-}  
+        k = n >> c;
+        if (k & 1)
+        {
+            printf("1");
+        }
+        else
+        {
+            printf("0");
+        }
+    c--;
+    }
+
+    printf("\n");
+}
