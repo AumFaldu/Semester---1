95b0ac4d9f4081d584234787a9669cef8c4157f3
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2023-10-04 09:28:20 +0530

    Add files via upload

---
+++ ArrayCountDuplicateElements.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main() 
+{
+    int i,n,j,count=0;
+    printf("Enter value of n : ");
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        printf("%d Number = ",i);
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        if(arr[i]==arr[j])
+        {
+            count = count + 1;
+        }
+    }
+    printf("Total Duplicate elements = %d",count);
+}
