e196223c058552745159aae9e074cabc52dee814
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2023-10-04 18:14:30 +0530

    Update ForFrequencyOfDigits.c

--- ForFrequencyOfDigits.c
+++ ForFrequencyOfDigits.c
@@ -1,16 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-   int i,d,n,count=0;
-   printf("Enter number and digit : ");
-   scanf("%d,%d",&n,&d);
-   for(i=1;n>0;i++)
-   {
-       if(n%10==d)
-       {
-       	count = count+1;
-       }
-     n/=10;
-   }
-   printf("Frequency of digits = %d",count);
+    int n,i=0,dig=0,j,k,count=0;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    j=n;
+    for(dig=0;n!=0;dig++)
+    {
+        n/=10;
+    }
+    int arr[dig];
+    for(i=0;i<dig;i++)
+    {
+        arr[i] = j%10;
+        j/=10;
+    }
+    for(k=0;k<=9;k++)
+    {   count = 0;
+        for(i=0;i<dig;i++)
+        {
+            if(arr[i] == k)
+            {
+                count = count + 1;
+            }
+        }
+        printf("Frequency of %d = %d\n",k,count);
+    }
 }
