316c62d14e102df69c3a6b212ff6af79b80c04a1
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2023-10-10 20:56:55 +0530

    Update 15-A-5.c

--- 15-A-5.c
+++ 15-A-5.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
-#include<string.h>
 void main()
-{    
+{
     char str[100];
-    int i = 0;
+    int i,count=0;
+    printf("Enter a string of length from (0-99) : ");
+    gets(str);
+    puts(str);
+    for(i=0;str[i]!='\0';i++)
+    {
+        count++;
+    }
+    printf("Length of String = %d",count);
     
-        printf("Enter a string : ");
-        scanf("%s",str);
-        printf("\n %s",str);
-        for(i=0;str[i]!='\0';i++);
-        printf("\nLength of string = %d",i);
-        
 }
