1243a784de0f63c4eddf73d8647fe7198b14eced
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2023-10-18 19:18:17 +0530

    Update 17-A-2.c

--- 17-A-2.c
+++ 17-A-2.c
@@ -1,11 +1,4 @@
 #include<stdio.h>
-void main()
-{
-    int *a,x;
-    a = &x;
-    printf("Enter value of x = ");
-    scanf("%d",&x);
-    printf("Value = %d\n Address = %d",x,a);#include<stdio.h>
                                                                                 
 void main()                                                                     
 {                                                                               
