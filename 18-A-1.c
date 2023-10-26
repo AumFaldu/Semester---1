daf9831088df0d5c84e8dec31feb8263a45c3755
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2023-10-26 12:57:14 +0530

    Update 18-A-1.c

--- 18-A-1.c
+++ 18-A-1.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
-int add();
+void add(int,int);
 void main()
 {
-    add();
-}
-int add()
-{
-      int a,b;
+    int a,b;
     printf("Enter integer 1 : ");
     scanf("%d",&a);
     printf("Enter integer 2 : ");
     scanf("%d",&b);
+    add(a,b);
+}
+void add(int a,int b)
+{
+    
     printf("Sum = %d",a+b);
     
 }
