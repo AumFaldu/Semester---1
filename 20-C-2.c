6f2a9f9c58746eb34735c2aaf20961aaf2cd2e50
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2023-11-08 09:22:02 +0530

    Update 20-C-2.c

--- 20-C-2.c
+++ 20-C-2.c
@@ -8,18 +8,10 @@ struct s
 void main()
 {
     int m,s;
-    printf("Enter Seconds of time 1 : ");
-    scanf("%d",&t1.ss);
-    printf("Enter Seconds of time 2 : ");
-    scanf("%d",&t2.ss);
-    printf("Enter Minutes of time 1 : ");
-    scanf("%d",&t1.mm);
-    printf("Enter Minutes of time 2 : ");
-    scanf("%d",&t2.mm);
-    printf("Enter Hours of time 1 : ");
-    scanf("%d",&t1.hh);
-    printf("Enter Hours of time 2 : ");
-    scanf("%d",&t2.hh);
+    printf("\nEnter Time 1 = ");
+    scanf("%d %d %d",&t1.hh,&t1.mm,&t1.ss);
+    printf("\nEnter Time 2 = ");
+    scanf("%d %d %d",&t2.hh,&t2.mm,&t2.ss);
     t3.ss = t1.ss + t2.ss;
     t3.mm = t1.mm + t2.mm;
     t3.hh = t1.hh + t2.hh;
@@ -37,4 +29,3 @@ void main()
     }
     printf("\nSum of Time = %d : %d : %d ",t3.hh,t3.mm,t3.ss);
 }
-
