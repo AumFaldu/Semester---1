504ee5f9cfe75986abe43fae3f86b00bf89d90c3
AumFaldu <140812087+AumFaldu@users.noreply.github.com>
2023-11-19 14:22:11 +0530

    Update 18-B-1.c

--- 18-B-1.c
+++ 18-B-1.c
@@ -1,24 +1,31 @@
 #include<stdio.h>
-void fibbo(int);
+int fibbo(int);
 void main()
 {
-    int n;
-   printf("Enter total number of digits = ");
-   scanf("%d",&n);
-   printf("Fibbonacci series : \n");
-   fibbo(n);
-}
-void fibbo(int a)
-{
-    int i=0,t1=0,t2=1,next_term=0;
-    printf("%d,",t1);
-    printf("%d",t2);
-    next_term= t1+t2; 
-    for(i=2;i<a;i++)
+    int i,n;
+    printf("Enter value of n : ");
+    scanf("%d",&n);
+    printf("Fibonacci Series : \n");
+    for(i=1;i<=n;i++)
     {
-        printf(",%d",next_term);
-      t1 = t2;
-      t2 = next_term;
-     next_term = t1+t2; 
+        printf("%d ",fibbo(i));
+    }
+}
+int fibbo(int n)
+{
+    if(n==1)
+    {
+        return 0;
+
+    }
+    if(n==2)
+    {
+        return 1;
+
+    }
+    else
+    {
+        return fibbo(n-2) + fibbo(n-1);
+
     }
 }
